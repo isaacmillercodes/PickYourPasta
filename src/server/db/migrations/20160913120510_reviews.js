a07d567a6de09bb8b85dd25acdc8777461e1e5ab
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-09-13 12:35:58 -0600

    created reviews migration

--- src/server/db/migrations/20160913120510_reviews.js
+++ src/server/db/migrations/20160913120510_reviews.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  knex.schema.createTable('reviews', (table) => {
+    table.increments();
+    table.integer('rest_id').references('restaurants.id');
+    table.integer('user_id').references('users.id');
+    table.text('review').notNullable();
+    table.integer('rating').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.dropTable('reviews');
 };

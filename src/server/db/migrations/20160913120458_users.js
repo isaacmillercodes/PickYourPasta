8ab6554850cc97787527c9e6ac55882158f1d34e
Sam Goldsmith <samggold@gmail.com>
2016-09-13 12:49:25 -0600

    update to user migration file

--- src/server/db/migrations/20160913120458_users.js
+++ src/server/db/migrations/20160913120458_users.js
@@ -1,8 +1,15 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.boolean('is_admin').notNullable();
+    table.string('email').notNullable();
+    table.password('password').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('users');
 };

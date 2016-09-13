9ee159eda47a25c370b8b43bf284556a39638908
Derek Styer <dmstyer@gmail.com>
2016-09-13 12:22:36 -0600

    added employee migrations

--- src/server/db/migrations/20160913120504_employees.js
+++ src/server/db/migrations/20160913120504_employees.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable(('employees'), (table) => {
+    table.increments();
+    table.string('first_name');
+    table.string('last_name');
+    table.string('title');
+    table.integer('rest_id');
+
+  });
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('employees');
 };

c585be100ab00f02398f1caef2c383c5ae3f020f
Sam Goldsmith <samggold@gmail.com>
2016-09-14 11:52:47 -0600

    update to user routes for login and signup

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/login', (req, res, next) => {
   res.render('users/login');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('users/signup');
 });
 
 module.exports = router;

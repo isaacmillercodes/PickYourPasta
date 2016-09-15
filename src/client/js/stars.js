661a70a5675d042cafb6934cfa3bc917b9d8ccc9
Sam Goldsmith <samggold@gmail.com>
2016-09-15 16:36:43 -0600

    add js file to do stars

---
+++ src/client/js/stars.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    var rating = $('.star-rating').data('rating');
+    console.log(rating);
+});
+
+// function countFullstars
+//
+//
+//
+// $.fn.stars = function() {
+//         return $(this).each(function() {
+//
+//             var rating = $(this).data("rating");
+//
+//             var numStars = $(this).data("numStars");
+//
+//             var fullStar = new Array(Math.floor(rating + 1)).join('<i class="fa fa-star"></i>');
+//
+//             var halfStar = ((rating%1) !== 0) ? '<i class="fa fa-star-half-empty"></i>': '';
+//
+//             var noStar = new Array(Math.floor(numStars + 1 - rating)).join('<i class="fa fa-star-o"></i>');
+//
+//             $(this).html(fullStar + halfStar + noStar);
+//
+//         });
+//     }
+//
+//     $('.stars').stars();
+// //

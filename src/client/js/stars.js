75322d7bc069fdb298f39623771671ef075e0205
Sam Goldsmith <samggold@gmail.com>
2016-09-16 09:18:10 -0600

    work on stars function

--- src/client/js/stars.js
+++ src/client/js/stars.js
@@ -1,29 +1,18 @@
 $(document).ready(function() {
-    var rating = $('.star-rating').data('rating');
-    console.log(rating);
-});
+  var rating = $('.star-rating').data('rating');
+  console.log(rating);
 
-// function countFullstars
-//
-//
-//
-// $.fn.stars = function() {
-//         return $(this).each(function() {
-//
-//             var rating = $(this).data("rating");
-//
-//             var numStars = $(this).data("numStars");
-//
-//             var fullStar = new Array(Math.floor(rating + 1)).join('<i class="fa fa-star"></i>');
-//
-//             var halfStar = ((rating%1) !== 0) ? '<i class="fa fa-star-half-empty"></i>': '';
-//
-//             var noStar = new Array(Math.floor(numStars + 1 - rating)).join('<i class="fa fa-star-o"></i>');
-//
-//             $(this).html(fullStar + halfStar + noStar);
-//
-//         });
-//     }
-//
-//     $('.stars').stars();
-// //
+function stars() {
+  return $('.stars').each(function() {
+    var rating = $('.stars').data("rating");
+    var numStars = $('.stars').data("numStars");
+    var fullStar = new Array(Math.floor(rating + 1)).join('<i class="fa fa-star"></i>');
+    var halfStar = ((rating % 1) !== 0) ? '<i class="fa fa-star-half-empty"></i>': '';
+    var noStar = new Array(Math.floor(numStars + 1 - rating)).join('<i class="fa fa-star-o"></i>');
+    $('.stars').html(fullStar + halfStar + noStar);
+  });
+}
+
+  $('.stars').stars();
+
+});
